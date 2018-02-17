e9443e7a69c277fa3e804638a72705ad7fbe62e7
Rosen Matev <rosen.matev@cern.ch>
2018-02-17 16:23:14 +0100

    Execute get_tree_entries in another process

--- gutils/merge.py
+++ gutils/merge.py
@@ -5,58 +5,9 @@ from collections import defaultdict
 import Ganga
 from utils import subjobs, outputfiles
 from download import download_files, get_access_urls
+from root_utils import get_tree_enties
 
 logger = Ganga.Utility.logging.getLogger('gutils.merge')
-
-
-def _get_trees(x, dir_name=""):
-    """Recursively get trees from x.
-       x can be a TFile or a TDirectoryFile
-       Returns a set of tuples: set( (tree_name, tree_object) )
-       Prepends the name of the object with dir_name
-       (dir_name should include a trailing /)
-    """
-    trees = set()
-    keys = set(key.GetName() for key in x.GetListOfKeys())
-    for key in keys:
-        obj = x.Get(key)
-        class_name = obj.IsA().GetName()
-        if class_name == "TTree":
-            trees.add((dir_name+obj.GetName(), obj))
-        elif class_name == "TDirectoryFile":
-            trees = trees.union(_get_trees(obj, dir_name+obj.GetName()+"/"))
-    return trees
-
-
-def _get_entries(files, ignore_empty=False, ignore_missing=False):
-    """Get number of entries of all trees in files
-       Returns a dictionary: {"tree_name":tree_entries}
-       tree_name includes the directory name(s), if applicable
-    """
-    from ROOT import TFile
-    from collections import defaultdict
-    if not hasattr(files, "__iter__"):  # allow single filename to be passed
-        files = [files]
-    entries = defaultdict(int)
-    for f in files:
-        file0 = TFile.Open(f)
-        if not file0:
-            if ignore_missing:
-                print "Warning: Can't find/open file: "+f
-                continue
-            raise IOError("Can't find/open file: "+f)
-
-        trees = _get_trees(file0)
-        if not trees:
-            if ignore_empty:
-                print 'Warning: No TTree objects found in '+f
-                continue
-            raise ValueError('No TTree objects found in '+f)
-
-        for name, tree in trees:
-            entries[name] += tree.GetEntries()
-        file0.Close("R")
-    return entries
 
 
 def _merge_root(inputs, output):
@@ -71,7 +22,7 @@ def _merge_root(inputs, output):
 
     config['ROOT']['version'] = old_ver
 
-    assert _get_entries(inputs) == _get_entries(output)
+    assert get_tree_enties(inputs) == get_tree_enties(output)
 
 
 def _prepare_merge(jobs, name, path, overwrite=False, partial=False):
---
+++ gutils/root_utils.py
@@ -0,0 +1,62 @@
+import subprocess
+
+def _get_trees(x, dir_name=""):
+    """Recursively get trees from x.
+       x can be a TFile or a TDirectoryFile
+       Returns a set of tuples: set( (tree_name, tree_object) )
+       Prepends the name of the object with dir_name
+       (dir_name should include a trailing /)
+    """
+    trees = set()
+    keys = set(key.GetName() for key in x.GetListOfKeys())
+    for key in keys:
+        obj = x.Get(key)
+        class_name = obj.IsA().GetName()
+        if class_name == "TTree":
+            trees.add((dir_name+obj.GetName(), obj))
+        elif class_name == "TDirectoryFile":
+            trees = trees.union(_get_trees(obj, dir_name+obj.GetName()+"/"))
+    return trees
+
+
+def _get_tree_entries(files, ignore_empty=False, ignore_missing=False):
+    """Get number of entries of all trees in files
+       Returns a dictionary: {"tree_name":tree_entries}
+       tree_name includes the directory name(s), if applicable
+    """
+    from ROOT import TFile
+    from collections import defaultdict
+    if not hasattr(files, "__iter__"):  # allow single filename to be passed
+        files = [files]
+    entries = defaultdict(int)
+    for f in files:
+        file0 = TFile.Open(f)
+        if not file0:
+            if ignore_missing:
+                print "Warning: Can't find/open file: "+f
+                continue
+            raise IOError("Can't find/open file: "+f)
+
+        trees = _get_trees(file0)
+        if not trees:
+            if ignore_empty:
+                print 'Warning: No TTree objects found in '+f
+                continue
+            raise ValueError('No TTree objects found in '+f)
+
+        for name, tree in trees:
+            entries[name] += tree.GetEntries()
+        file0.Close("R")
+    return dict(entries)
+
+
+def get_tree_enties(files, ignore_empty=False, ignore_missing=False):
+    cmd = '_get_tree_entries({!r}, {!r}, {!r})'.format(files, ignore_empty, ignore_missing)
+    out = subprocess.check_output(['lb-run', 'ROOT', 'python', __file__, cmd])
+    return eval(out)
+
+
+if __name__ == '__main__':
+    import sys
+    cmd = sys.argv[1]
+    print repr(eval(cmd))
